e48edf607c1fed1c0bede35f5e0228b02e19a051
Huong <you@example.com>
2018-01-31 18:40:02 +0700

    detail

--- controller/DetailFoodController.php
+++ controller/DetailFoodController.php
@@ -1,12 +1,22 @@
 <?php
 include_once 'Controller.php';
+include_once 'models/DetailFoodModel.php';
+
 class DetailFoodController extends Controller{
     function getDetail(){
         $id = $_GET['id'];
         $alias = $_GET['alias'];
-        echo $id;
-        echo $alias;
-        //return $this->loadView('chitiet',"Chi tiết");
+        
+        $model = new DetailFoodModel;
+        $food = $model->getDetailFood($id,$alias);
+
+        // echo "<pre>";
+        // print_r($food);
+        // echo "</pre>";
+        $data = [
+            'food'=>$food
+        ];
+        return $this->loadView('chitiet',"Chi tiết",$data);
     }
 }
 
---
+++ models/DetailFoodModel.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'DBConnect.php';
+class DetailFoodModel extends DBConnect{
+    function getDetailFood($id,$alias){
+        $sql = "SELECT f.*, p.url 
+                FROM foods f
+                INNER JOIN page_url p
+                ON f.id_url = p.id
+                WHERE f.id=$id
+                AND url= '$alias' ";
+        return $this->loadOneRow($sql);
+    }
+}
