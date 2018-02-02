a999ed0ad884021978ecf47cd7605bf4942447bb
Huong <you@example.com>
2018-02-02 18:45:13 +0700

    type

--- models/TypeModel.php
+++ models/TypeModel.php
@@ -3,7 +3,15 @@ include_once 'models/DBConnect.php';
 
 class TypeModel extends DBConnect{
     function getAllType(){
-        $sql = "SELECT * FROM food_type";
+        $sql = "SELECT * FROM food_type ORDER BY id ASC";
+        return $this->loadMoreRows($sql);
+    }
+    function getFoodsFirstType(){
+        $sql = "SELECT f.*, p.url 
+                FROM foods f
+                INNER JOIN page_url p
+                ON f.id_url = p.id  
+                WHERE id_type=(SELECT t.id FROM food_type t ORDER BY t.id ASC LIMIT 0,1)";
         return $this->loadMoreRows($sql);
     }
 }
